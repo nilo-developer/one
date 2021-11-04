3603b3100aa9d787f0adaf889787acab4ab3666b
niloo <80812934+nilo-developer@users.noreply.github.com>
2021-11-04 12:49:57 +0330

    expandable

--- app/src/main/java/com/example/myapplication/adapter/Adapter.java
+++ app/src/main/java/com/example/myapplication/adapter/Adapter.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -11,6 +11,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.R;
+import com.example.myapplication.Seminar;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
